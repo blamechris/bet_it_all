3a387368533f9227b4fc3e162153b4d7ab0e6a49
Christopher <46629049+blamechris@users.noreply.github.com>
2022-12-10 17:59:14 -0800

    Minor change

--- bot_main_old.py
+++ bot_main_old.py
@@ -22,9 +22,30 @@ async def on_ready():
     print(f'Logged in as {bot.user} (ID: {bot.user.id})')
     print('------')
 
-@ bot.command()
+@bot.command()
 async def add(ctx, left: int, right: int):
     """Adds two numbers together."""
     await ctx.send(left + right)
+
+@bot.command()
+async def poll(ctx, option1: str, option2: str):
+    poll_message = await ctx.channel.send(f'Poll: {option1} or {option2}?')
+    await poll_message.add_reaction('🔴') # Red emoji
+    await poll_message.add_reaction('🔵') # Blue emoji
+
+@bot.event
+async def on_reaction_add(reaction, user):
+    if user == bot.user:
+        return
+
+    # Check if the reaction is a red or blue emoji
+    if str(reaction.emoji) == '🔴':
+        # Record the user as picking option 1
+        print(f'{user.name} picked option 1')
+    elif str(reaction.emoji) == '🔵':
+        # Record the user as picking option 2
+        print(f'{user.name} picked option 2')
+
+    
 
 bot.run(TOKEN)
